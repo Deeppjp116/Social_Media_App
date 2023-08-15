ccfc51ddef71ff3c2dd9124d24439a801ebaf2df
deep <deeppjp1656@gmail.com>
2023-08-15 11:35:58 +0530

    add some redux

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -13,7 +13,27 @@ export const authSlice = createSlice({
   initialState,
   reducers: {
     setMode: (state) => {
-      state.mode = state.mode === 'light';
+      state.mode = state.mode === 'light' ? 'dark' : 'light';
     },
+    setLogIn: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error('The User friends non-existent :(');
+      }
+    },
+    setPosts:(state,action)=>{
+        state.token = action.payload.token;
+
+    }
   },
 });
